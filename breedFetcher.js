8176ec05b49e7ec9d683485b28d31e3a90cc9a6c
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-23 15:54:02 -0500

    Updates were made to breedFetcher.js file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
 const needle = require('needle');
-const [breedName] = process.argv.slice(2);
-needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
-  if (err) {
-    console.log('An error occured while requesting data from server', err);
-    return;
-  }
-  // console.log('statusCode:', response && response.statusCode);
-  //console.log('body:' , body);
-  // console.log(typeof body);
-  if (Array.isArray(body) && body.length > 0) {
-    console.log('Description of breed requested:', body[0].description);
-  } else {
-    console.log('No data found for requested breed');
-  }
 
-});
+const fetchBreedDescription = function (breedName, callback) {
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    // console.log('statusCode:', response && response.statusCode);
+    //console.log('body:' , body);
+    // console.log(typeof body);
+    if (Array.isArray(body) && body.length > 0) {
+      callback(null, body[0].description);
+    } else {
+      callback(null, 'No data found for requested breed');
+    }
+
+  });
+};
+
+module.exports = { fetchBreedDescription };
